1590963c9098d0f75c6a0e28eade8b89c6c0274d
Ryan Yuson <113479730+ryuson01@users.noreply.github.com>
2023-07-24 15:41:11 -0500

    Update Beatles_WebScraping.R

--- Beatles_WebScraping.R
+++ Beatles_WebScraping.R
@@ -1,24 +1,21 @@
-#Jackson Kithcart and Ryan Yuson
-#Group Project Web Scraping
-#12/9/2022
-
 rm(list=ls())
 
+# Load packages
 library(tidyr)
 library(rvest)
 library(stringr)
 library(dplyr)
 
-###Read kaggle csv into data frame
+# Read kaggle csv into data frame
 Beatles <- read.csv("TheBeatlesCleaned.csv")
 
-###Grab website url
+# Grab website url
 url <- paste("https://en.wikipedia.org/wiki/List_of_songs_recorded_by_the_Beatles")
 
-###Parse website and grab table
+# Parse website and grab table
 file<-read_html(url)
 tables<-html_nodes(file, "table")
 table1 <- html_table(tables[3], fill = TRUE)
 
-#put table into data frame
+# Put table into data frame
 songs <- table1[[1]]
